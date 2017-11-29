169bfd29dc11cbfb748446d2b96bff2b6d8241b3
WangYiNing <we820731@gmail.com>
2017-11-29 10:35:33 +0800

    first commits

---
+++ resources/lang/zh-TW/passwords.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+    //Passwords must be at least six characters and match the confirmation.
+    'password' => '密碼至少須要6碼以上並確認與密碼相符',
+    //Your password has been reset!
+    'reset' => '您的密碼已重置!',
+    //We have e-mailed your password reset link!
+    'sent' => '我們已經透過電子郵件發送您的密碼重置連結!',
+    //This password reset token is invalid.
+    'token' => '此密碼重置無效',
+    //We can't find a user with that e-mail address.
+    'user' => "我們無法找到該電子郵件的用戶",
+
+];
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>大創娃娃屋 | @yield('title')</title>
+	<head>
+
+
+
+
+</html>
+
+
+
+
