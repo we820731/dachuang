9b1de1991bd27a031ea27810c8a5b4e731e8d36f
WangYiNing <we820731@gmail.com>
2017-12-04 10:08:34 +0800

    更新Users產生器中文化

--- app/Http/Controllers/AdminCmsUsersController.php
+++ app/Http/Controllers/AdminCmsUsersController.php
@@ -20,19 +20,19 @@ class AdminCmsUsersController extends \crocodicstudio\crudbooster\controllers\CB
 	
 		# START COLUMNS DO NOT REMOVE THIS LINE
 		$this->col = array();
-		$this->col[] = array("label"=>"Name","name"=>"name");
+		$this->col[] = array("label"=>"姓名","name"=>"name");
 		$this->col[] = array("label"=>"Email","name"=>"email");
-		$this->col[] = array("label"=>"Privilege","name"=>"id_cms_privileges","join"=>"cms_privileges,name");
-		$this->col[] = array("label"=>"Photo","name"=>"photo","image"=>1);		
+		$this->col[] = array("label"=>"權限","name"=>"id_cms_privileges","join"=>"cms_privileges,name");
+		$this->col[] = array("label"=>"大頭照","name"=>"photo","image"=>1);		
 		# END COLUMNS DO NOT REMOVE THIS LINE
 
 		# START FORM DO NOT REMOVE THIS LINE
 		$this->form = array(); 		
-		$this->form[] = array("label"=>"Name","name"=>"name",'required'=>true,'validation'=>'required|alpha_spaces|min:3');
+		$this->form[] = array("label"=>"姓名","name"=>"name",'required'=>true,'validation'=>'required|alpha_spaces|min:3');
 		$this->form[] = array("label"=>"Email","name"=>"email",'required'=>true,'type'=>'email','validation'=>'required|email|unique:cms_users,email,'.CRUDBooster::getCurrentId());		
-		$this->form[] = array("label"=>"Photo","name"=>"photo","type"=>"upload","help"=>"Recommended resolution is 200x200px",'required'=>true,'validation'=>'required|image|max:1000');											
-		$this->form[] = array("label"=>"Privilege","name"=>"id_cms_privileges","type"=>"select","datatable"=>"cms_privileges,name",'required'=>true);						
-		$this->form[] = array("label"=>"Password","name"=>"password","type"=>"password","help"=>"Please leave empty if not change");
+		$this->form[] = array("label"=>"大頭照","name"=>"photo","type"=>"upload","help"=>"推薦尺寸大小為 200x200px",'required'=>true,'validation'=>'required|image|max:1000');											
+		$this->form[] = array("label"=>"權限","name"=>"id_cms_privileges","type"=>"select","datatable"=>"cms_privileges,name",'required'=>true);						
+		$this->form[] = array("label"=>"密碼","name"=>"password","type"=>"password","help"=>"密碼未變更請留空");
 		# END FORM DO NOT REMOVE THIS LINE
 				
 	}
