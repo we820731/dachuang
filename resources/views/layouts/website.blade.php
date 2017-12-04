191f8cad383e410abd878f007b38e4d9c2571a82
WangYiNing <we820731@gmail.com>
2017-12-04 11:55:30 +0800

    新增分頁icon

--- resources/views/layouts/website.blade.php
+++ resources/views/layouts/website.blade.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <title>大創娃娃屋 |  @yield('title')</title>
+        <link rel="shortcut icon" href="images/sm-icon.png" />
     </head>
     <body>
     	@include('website.partials.navigation')
